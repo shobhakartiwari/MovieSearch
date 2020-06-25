ebd65851a87ddf1cba2b9a209dcd75d4edd84748
Jiaxing Liu <275529599@qq.com>
2020-06-25 13:48:56 -0500

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by jiaxing liu on 6/25/20.
+//  Copyright © 2020 jiaxing liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
